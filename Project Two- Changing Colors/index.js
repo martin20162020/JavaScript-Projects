1a638aff86afc526635cf8947888375a1c55132c
vicky <vcmartin2020@gmail.com>
2021-03-25 13:45:49 -0500

    Project Two- Changing Colors

---
+++ Project Two- Changing Colors/index.js
@@ -0,0 +1,19 @@
+function ChangeColorPink(){
+    document.body.style.background = "#F3CFC6"
+}
+
+function ChangeColorGreen(){
+    document.body.style.background = "#DEFFE2"
+}
+
+function ChangeColorBlue(){
+    document.body.style.background = "#C5DCF8"
+}
+
+function ChangeColorOrange(){
+    document.body.style.background = "#F3CCB0"
+}
+
+function ChangeColorPurple(){
+    document.body.style.background = "#DCD4F8"
+}
