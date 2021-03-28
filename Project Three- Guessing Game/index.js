72b8a9e5f664664158c1af88c333c0320f6a68c5
vicky <vcmartin2020@gmail.com>
2021-03-27 22:31:09 -0500

    Fixed minor issues: added semicolons & added to the last else if statement

--- Project Three- Guessing Game/index.js
+++ Project Three- Guessing Game/index.js
@@ -3,19 +3,25 @@ let guessCount = 1;
 
 document.getElementById("submitguess").onclick = function(){
     let numberGuessed = document.getElementById("guessField").value;
-    console.log(randomNumber)
+    console.log(randomNumber);
     if (numberGuessed == randomNumber){
         alert(`you got it in ${guessCount} guess(es)`)
     } else if(numberGuessed > randomNumber && guessCount < 3){
-        alert(`The number is LESS than ${numberGuessed}`)
+        alert(`The number is LESS than ${numberGuessed}`);
         ++guessCount;
     } else if (numberGuessed < randomNumber && guessCount < 3){
-        alert(`The number is GREATER than ${numberGuessed}`)
+        alert(`The number is GREATER than ${numberGuessed}`);
         ++guessCount;
     } else if (numberGuessed < 1  || numberGuessed  > 100){
-        alert(`INVALID! ${numberGuessed} is not between 1 - 100!`)
+        if(guessCount < 3){
+            alert(`INVALID! ${numberGuessed} is not between 1 - 100!`);
+            ++guessCount;
+        }
+        else{
+            alert(`Sorry! You ran out of guesses, the number was: ${randomNumber}`);
+        }
     }
     else{
-        alert(`Sorry! You ran out of guesses, the number was: ${randomNumber}`)
+        alert(`Sorry! You ran out of guesses, the number was: ${randomNumber}`);
     }
 }
