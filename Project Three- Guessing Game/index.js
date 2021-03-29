3b0f2c3ede4d343e1136d8a5ca172d5771401fb8
vicky <vcmartin2020@gmail.com>
2021-03-29 10:17:41 -0500

    Created two new functions & fixed minor bugs

--- Project Three- Guessing Game/index.js
+++ Project Three- Guessing Game/index.js
@@ -5,23 +5,45 @@ document.getElementById("submitguess").onclick = function(){
     let numberGuessed = document.getElementById("guessField").value;
     console.log(randomNumber);
     if (numberGuessed == randomNumber){
-        alert(`you got it in ${guessCount} guess(es)`)
-    } else if(numberGuessed > randomNumber && guessCount < 3){
-        alert(`The number is LESS than ${numberGuessed}`);
-        ++guessCount;
-    } else if (numberGuessed < randomNumber && guessCount < 3){
-        alert(`The number is GREATER than ${numberGuessed}`);
-        ++guessCount;
+        alert (`you got it in ${guessCount} guess(es)`);
+        answeredIt();
     } else if (numberGuessed < 1  || numberGuessed  > 100){
         if(guessCount < 3){
             alert(`INVALID! ${numberGuessed} is not between 1 - 100!`);
+            document.getElementById("guessField").value = "";
             ++guessCount;
         }
         else{
             alert(`Sorry! You ran out of guesses, the number was: ${randomNumber}`);
+            answeredIt()
         }
-    }
+    } else if(numberGuessed > randomNumber && guessCount < 3){
+        alert(`The number is LESS than ${numberGuessed}`);
+        document.getElementById("guessField").value = "";
+        ++guessCount;
+    } else if (numberGuessed < randomNumber && guessCount < 3){
+        alert(`The number is GREATER than ${numberGuessed}`);
+        document.getElementById("guessField").value = "";
+        ++guessCount;
+    } 
     else{
         alert(`Sorry! You ran out of guesses, the number was: ${randomNumber}`);
+        answeredIt();
     }
+}
+
+answeredIt = () =>{
+    randomNumber = Math.floor(Math.random() * 100 + 1)
+    guessCount = 1
+    document.getElementById("guessField").value = ""
+    yesOrNo()
+}
+
+yesOrNo = () =>{
+ let txt;
+ if (confirm("Would you like to play again?")){
+     alert("Guess a number between 1-100!");
+ } else {
+     alert("Thanks for playing!");
+ }
 }
