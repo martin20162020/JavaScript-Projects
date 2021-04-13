7a31e88e8d7207ac75e32171a769232975b10e8a
vicky <vcmartin2020@gmail.com>
2021-04-12 21:19:25 -0500

    edited Project 7

--- Project 7- Tip Calculator/index.js
+++ Project 7- Tip Calculator/index.js
@@ -1,14 +1,42 @@
 const tips = () =>{
-    let totalValue = document.querySelector("#total")
+    const fifteenTip = document.querySelector("#fifteen")
+    const twentyTip = document.querySelector("#twenty")
+    const twentyFiveTip = document.querySelector("#twentyFive")
+    const noTip = document.querySelector("#noTip")
 
-    document.querySelector("#fifteen").onclick = fifteenTip = ()=> totalValue.textContent = `$${(parseInt(document.querySelector("#tip").value) * 1.15).toFixed(2)}`
+    const totalValue = document.querySelector("#total")
 
-    document.querySelector("#twenty").onclick = twentyTip = ()=> totalValue.textContent = `$${(parseInt(document.querySelector("#tip").value) * 1.20).toFixed(2)}`
+    fifteenTip.addEventListener("click", fifteenPercent = ()=> {
+        if ((document.querySelector("#tip").value === "")) {
+            totalValue.textContent = "NO VALUE GIVEN"
+        } else{
+            totalValue.textContent = `$${(parseInt(document.querySelector("#tip").value) * 1.15).toFixed(2)}`
+        }
+    })
 
-    document.querySelector("#twentyFive").onclick = twentyFiveTip = () => totalValue.textContent = `$${(parseInt(document.querySelector("#tip").value) * 1.25).toFixed(2)}`
+    twentyTip.addEventListener("click", twentyPercent = () => {
+        if ((document.querySelector("#tip").value === "")) {
+            totalValue.textContent = "NO VALUE GIVEN"
+        } else{
+            totalValue.textContent = `$${(parseInt(document.querySelector("#tip").value) * 1.20).toFixed(2)}`
+        }
+    })
 
-    document.querySelector("#noTip").onclick =noTip = ()=> totalValue.textContent = `$${parseInt(document.querySelector("#tip").value).toFixed(2)}`
+    twentyFiveTip.addEventListener("click", twentyFivePercent = () => {
+        if ((document.querySelector("#tip").value === "")){
+        totalValue.textContent = "NO VALUE GIVEN"
+        } else{
+        totalValue.textContent = `$${(parseInt(document.querySelector("#tip").value) * 1.25).toFixed(2)}`
+        }
+    })
 
+    noTip.addEventListener("click", zeroPercent = () => {
+        if ((document.querySelector("#tip").value === "")){
+            totalValue.textContent = "NO VALUE GIVEN"
+        } else{
+            totalValue.textContent = `$${(parseInt(document.querySelector("#tip").value)).toFixed(2)}`      
+        }
+    })
 }
 
 tips()
