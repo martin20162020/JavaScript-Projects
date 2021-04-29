fd9e4a0be494c0f332a28b7763b071440502a347
vicky <vcmartin2020@gmail.com>
2021-04-29 14:47:32 -0500

    Project 10

--- Project 10-Random Recipe/index.js
+++ Project 10-Random Recipe/index.js
@@ -1,9 +1,39 @@
 //Attempting to get data
 tester = () =>{
-    axios('www.themealdb.com/api/json/v1/1/random.php')
+    axios('https://www.themealdb.com/api/json/v1/1/random.php')
     .then(response => {
-        console.log(response.meal)
+        // GRABBING DATA
+        randomMeal = response.data.meals[0]
+        document.querySelector(".recipes").innerHTML = randomMeal.strMeal.toUpperCase()
+        
+        // RANDOM IMAGE
+        document.querySelector(".changingImage").src = randomMeal.strMealThumb;
+
+        //RANDOM VIDEO
+        document.querySelector('iframe').src = `https://www.youtube.com/embed/${randomMeal.strYoutube.slice(-11)}`
+
+        //Instructions
+        const instructions = document.createElement("div")
+        const information = document.querySelector(".extra")
+        information.innerHTML = randomMeal.strInstructions
+
+        //Ingredients
+        const ingredients = document.createElement("div")
+        const unorderedList = document.querySelector("ul")
+        ingredients.classList.add = "ingredientContainer"
+        const allIngredients = []
+
+        for (let i = 1; i < 20; i++){
+            if (randomMeal[`strIngredient${i}`]){
+                let singleItem = `<br>${randomMeal[`strIngredient${i}`]} - ${randomMeal[`strMeasure${i}`]}`
+                allIngredients.push(singleItem)
+            } else {
+                break
+            }
+        }
+        unorderedList.innerHTML = allIngredients
     })
 }
 
 document.querySelector("button").addEventListener("click", tester)
+document.addEventListener("DOMContentLoaded", tester)
